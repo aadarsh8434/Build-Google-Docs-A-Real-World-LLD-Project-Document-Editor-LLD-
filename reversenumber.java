b9adde2d1394ff840d2127beb45f6330c7d62abe
aadarsh8434 <aadarshranjan8434@gmail.com>
2025-11-23 17:08:05 +0530

    first commit

---
+++ reversenumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class reversenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int reversed=0;
+        while(n>0) {
+            reversed= reversed*10 + (n%10);
+            n /=10;
+
+        }
+        System.out.println(reversed);
+
+    }
+}
